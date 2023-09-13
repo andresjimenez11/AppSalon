56a9ccdc9282503587a4d407f4452d98afe75ca9
Andrés Felipe Jiménez Sánchez <njimenezgiraldo@gmail.com>
2023-09-12 19:40:33 -0500

    deployment domcloud

--- src/js/app.js
+++ src/js/app.js
@@ -110,7 +110,7 @@ function paginaSiguiente() {
 
 async function consultarAPI() {
     try {
-        const url = '/api/servicios';
+        const url = `${location.origin}/api/servicios`;
         const resultado = await fetch(url);
         const servicios = await resultado.json();
         mostrarServicios(servicios);
@@ -348,7 +348,7 @@ async function reservarCita() {
 
     try {
         // Peticion hacia la api
-        const url = '/api/citas';
+        const url = `${location.origin}/api/citas`;
 
         const respuesta = await fetch(url, {
             method: 'POST',
